9c451e14fe89bb59a85b076e017e80ac7a1ba2b2
Kishor Krishna K <kishorkrishna3k@gmail.com>
2023-09-05 15:23:54 +0530

    auth page,job card done

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,7 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-
-export {Navbar, Footer}
+import SignUp from "./SignUp";
+import CustomButton from "./CustomButton";
+import ListBox from "./ListBox";
+import JobCard from "./JobCard";
+export {Navbar, Footer,SignUp,ListBox,CustomButton,JobCard}
